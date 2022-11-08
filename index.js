d00e64991bc910c4349e6cab192fa18562f64d3e
mdefat233 <kharifte16@gmail.com>
2022-11-08 22:15:56 +0600

    review api added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectID, ObjectId } = require('bson');
 require ('dotenv').config();
 const app = express()
 
@@ -19,6 +20,34 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run (){
     try{
        const serviceCollection = client.db('MrTraveller').collection('services');
+       
+       const reviewCollection = client.db('MrTraveller').collection('reviews');
+       app.get('/services', async(req, res) =>{
+        const query = {}
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+       });
+       app.get('/services/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id) };
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+       });
+
+    //    review api
+
+    app.get('/reviews', async (req, res) =>{
+        const query =  {};
+        const cursor = reviewCollection.find(query);
+        const reviews = await cursor.toArray();
+        res.send(reviews)
+    })
+    app.post('/reviews', async(req, res) =>{
+        const review = req.body;
+        const result = await reviewCollection.insertOne(review);
+        res.send(result);
+    })
     }
     finally{
 
