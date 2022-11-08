8f6a5a666b06fc23b80cf6451d172a5de3f4f535
mdefat233 <kharifte16@gmail.com>
2022-11-08 15:44:30 +0600

    mongodb server setup

--- index.js
+++ index.js
@@ -14,12 +14,18 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nrl9whk.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+
+async function run (){
+    try{
+       const serviceCollection = client.db('MrTraveller').collection('services');
+    }
+    finally{
+
+    }
+    
+}
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) =>{
     res.send('Mr Traveller server is running')
