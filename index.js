4489e1989001defca35b44e06e5fdf90e34dc6bd
mdefat233 <kharifte16@gmail.com>
2022-11-09 00:54:03 +0600

    update user api

--- index.js
+++ index.js
@@ -53,6 +53,19 @@ async function run (){
         const result = await reviewCollection.insertOne(review);
         res.send(result);
     })
+
+    app.patch('/reviews/:id', async (req, res) =>{
+        const id = req.params.id;
+        const status = req.body.status
+        const query = {_id: ObjectId(id)}
+        const updateReview = {
+            $set: {
+                status: status
+            }
+        }
+        const result = await reviewCollection.updateOne(query, updateReview);
+        res.send(result);
+    })
      app.delete('/reviews/:id', async(req, res) =>{
         const id = req.params.id;
         const query = {_id: ObjectId(id)};
